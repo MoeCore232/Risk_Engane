43bced152937d290974f9ff24892e605cf29f1b0
mohamedonsnap-sys <mohamedonsnap@gmail.com>
2026-08-12 10:24:31 +0300

    second version, i forgat what i did

--- src/main/java/com/example/Risk_Engane/Device/DeviceController.java
+++ src/main/java/com/example/Risk_Engane/Device/DeviceController.java
@@ -1,11 +1,7 @@
 package com.example.Risk_Engane.Device;
 
-import com.example.Risk_Engane.Auth.Auth;
-import com.example.Risk_Engane.Auth.AuthRepo;
-import com.example.Risk_Engane.ErrorHandling.CustomResponseException;
 import com.example.Risk_Engane.ErrorHandling.GlobalResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
